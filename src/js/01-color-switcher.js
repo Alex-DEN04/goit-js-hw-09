74ec8f1d9c9322700a082b81495ca59854fa42d4
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-05-12 19:59:13 +0200

    01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    body: document.querySelector('body'),
+    startButton: document.querySelector("[data-start]"),
+    stopButton: document.querySelector("[data-stop]"),
+}
 
+let timerId = null;
+
+refs.startButton.addEventListener("click", onStartButtonClick);
+refs.stopButton.addEventListener("click", onStopButtonClick)
+ 
+function onStartButtonClick() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    refs.startButton.disabled = true;
+    refs.stopButton.disabled = false;
+}
+
+function onStopButtonClick() {
+    clearInterval(timerId);
+    refs.stopButton.disabled = true;
+    refs.startButton.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
