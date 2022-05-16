db05a2dd86b5f1c87b318f8b918e0ec075c02609
Oleksandr Levchuk <emihrant.ua@gmail.com>
2022-05-16 20:21:55 +0200

    03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const refs = {
+  form: document.querySelector(".form"),
+}
+
+let delay = "";
+let step = "";
+let amount = "";
+
+refs.form.addEventListener("submit", onFormSabmit);
+
+function onFormSabmit(event) {
+  event.preventDefault();
+ 
+  step = event.target.step.value;
+  amount = event.target.amount.value;
+  delay = event.target.delay.value;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise([i], delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay = Number(delay) + Number(step);
+  } 
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve( {position, delay} );
+      } else {
+        reject( {position, delay} );
+      }
+    }, delay);
+  });
 }
